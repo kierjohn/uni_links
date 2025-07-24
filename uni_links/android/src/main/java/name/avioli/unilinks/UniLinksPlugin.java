aa86d05542863ecb1191355fe457102ecf7b1de3
kierjohnsagre@gmail.com <kierjohnsagre@gmail.com>
2025-07-24 08:31:44 +0800

    remove

--- uni_links/android/src/main/java/name/avioli/unilinks/UniLinksPlugin.java
+++ uni_links/android/src/main/java/name/avioli/unilinks/UniLinksPlugin.java
@@ -83,16 +83,7 @@ public class UniLinksPlugin
     /** Plugin registration. */
     public static void registerWith(@NonNull PluginRegistry.Registrar registrar) {
         // Detect if we've been launched in background
-        if (registrar.activity() == null) {
-            return;
-        }
-
-        final UniLinksPlugin instance = new UniLinksPlugin();
-        instance.context = registrar.context();
-        register(registrar.messenger(), instance);
-
-        instance.handleIntent(registrar.context(), registrar.activity().getIntent());
-        registrar.addNewIntentListener(instance);
+        
     }
 
     @Override
